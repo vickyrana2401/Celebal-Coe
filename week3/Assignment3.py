a1136493b316ec9a1d6dc841a359c6595a2b62cf
vickyrana2401 <vickyrana2429@gmail.com>
2024-06-02 21:44:13 +0530

    update

---
+++ week3/Assignment3.py
@@ -0,0 +1,29 @@
+# Creating 2 sub plots using matplot Subplot
+import matplotlib.pyplot as plt
+import numpy as np
+
+#plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 2, 1)
+plt.plot(x,y)
+
+#plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 2, 2)
+plt.plot(x,y)
+
+plt.show()
+
+# Creating Scatter Plot using matplot Scatter
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+
+plt.scatter(x, y)
+plt.show()
